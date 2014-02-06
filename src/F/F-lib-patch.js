111568d6ec7229191b420f4c8abae0824cca3aab
Jaime del Palacio <jaimedelpalacio@forio.com>
2014-02-06 11:19:56 -0800

    fixed f lib patch

--- src/F/F-lib-patch.js
+++ src/F/F-lib-patch.js
@@ -1,40 +1,45 @@
+(function () {
 
-// this file contains patches to the Forio libraries that are being retrofitted to previous
-// versions so two functions are replaced to handle & character when uploading users and passwords
-// version 1.14 of F lib contains this fixes
-if(F && F.API && F.UserGroup) {
-    F.API.UserGroup.add = function(userList, callback,  options, group){
-        var newurl =  (group) ?  url + "/" + group:  url;
+    // this file contains patches to the Forio libraries that are being retrofitted to previous
+    // versions so two functions are replaced to handle & character when uploading users and passwords
+    // version 1.14 of F lib contains this fixes
+    if(F && F.API && F.API.UserGroup) {
+        var userGroupUrl = F.APIUtils.getURL("usergroup");
 
-        // force APIConnection to use jQuery paramter parser instead of its own
-        // since it is more robust to handle special characters in names, passwords, etc.
-        var ac = new APIConnection(newurl, null, { parameterParser: $.param });
+        F.API.UserGroup.add = function(userList, callback,  options, group){
+            var newurl =  (group) ?  userGroupUrl + "/" + group: userGroupUrl;
 
-        var params = {
-            action: "addUsers",
-            content: userList
+            // force APIConnection to use jQuery paramter parser instead of its own
+            // since it is more robust to handle special characters in names, passwords, etc.
+            var ac = new APIConnection(newurl, null, { parameterParser: $.param });
+
+            var params = {
+                action: "addUsers",
+                content: userList
+            };
+
+            $.extend(params, options);
+            ac.post(params , callback); //Dry run to see if there are any errors
         };
+    }
 
-        $.extend(params, options);
-        ac.post(params , callback); //Dry run to see if there are any errors
-    };
-}
+    if(F && F.API && F.API.Auth) {
+        var url = function(){ return F.APIUtils.getURL("authentication"); };
+        F.API.Auth.login = function(email,password,callback, options){
+            var params = "user_action=login&email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);
 
-if(F && F.API && F.API.Auth) {
-    var url = function(){return F.APIUtils.getURL("authentication")};
-    F.API.Auth.login = function(email,password,callback, options){
-        var params = "user_action=login&email=" + encodeURIComponent(email) + "&password=" + encodeURIComponent(password);
+            var defaults = {
+                parameterParser: null,
+                onError: function(errorMess, errorThrown, responseText){
+                    var response = $.parseJSON(responseText);
+                    callback(response);
+                } //Call the login handler anyway with the status code
+            };
+            $.extend(defaults, options);
 
-        var defaults = {
-            parameterParser: null,
-            onError: function(errorMess, errorThrown, responseText){
-                var response = $.parseJSON(responseText);
-                callback(response);
-            } //Call the login handler anyway with the status code
+            var ac = new APIConnection(url, defaults.params , defaults);
+            return  ac.post(params , callback);
         };
-        $.extend(defaults, options);
+    }
 
-        var ac = new APIConnection(url, defaults.params , defaults);
-        return  ac.post(params , callback);
-    };
-}
+})();
